55c8ae30af5188d7e20f5d2ea0dfc8e44fc5f7fa
Raja Singh Samuel <rajasinghsamuelb@karunya.edu.in>
2020-08-27 22:29:55 +0530

    final version

--- app/src/main/java/com/urk17cs290/shoppingcart/BillActivity.java
+++ app/src/main/java/com/urk17cs290/shoppingcart/BillActivity.java
@@ -2,12 +2,10 @@ package com.urk17cs290.shoppingcart;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Objects;
+import java.text.MessageFormat;
 
 public class BillActivity extends AppCompatActivity {
 
@@ -16,21 +14,13 @@ public class BillActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_bill);
     Intent i = getIntent();
-    if (i.getStringExtra("A_COUNT") != null) Log.d("TAG", "a count not null ");
 
-    int aCount = 0;
-    aCount = Integer.parseInt(i.getStringExtra("A_COUNT"));
-    int oCount=0;
-    oCount = Integer.parseInt(i.getStringExtra("O_COUNT"));
-    int bCount=0;
-    bCount = Integer.parseInt(i.getStringExtra("B_COUNT"));
-    int mCount=0;
-    mCount = Integer.parseInt(i.getStringExtra("M_COUNT"));
-    int gCount=0;
-    gCount = Integer.parseInt(Objects.requireNonNull(i.getStringExtra("G_COUNT")));
-
-    int tot=0;
-    tot = Integer.parseInt(Objects.requireNonNull(i.getStringExtra("TOTAL")));
+    int aCount = i.getIntExtra("A_COUNT", 0);
+    int oCount = i.getIntExtra("O_COUNT", 0);
+    int bCount = i.getIntExtra("B_COUNT", 0);
+    int mCount = i.getIntExtra("M_COUNT", 0);
+    int gCount = i.getIntExtra("G_COUNT", 0);
+    int tot = i.getIntExtra("TOTAL", 0);
 
     TextView apple = findViewById(R.id.apples);
     TextView orange = findViewById(R.id.oranges);
@@ -39,12 +29,12 @@ public class BillActivity extends AppCompatActivity {
     TextView grape = findViewById(R.id.grapes);
     TextView total = findViewById(R.id.total);
 
-    apple.setText("APPLES (" + aCount + ") x 200 = " + aCount * 200);
-    orange.setText("ORANGES (" + oCount + ") x 80 = " + oCount * 200);
-    banana.setText("BANANA (" + bCount + ") x 50 = " + bCount * 200);
-    mango.setText("MANGOES (" + mCount + ") x 60 = " + mCount * 200);
-    grape.setText("GRAPES (" + gCount + ") x 100 = " + gCount * 200);
-    total.setText("Total : Rs" + tot);
+    apple.setText(MessageFormat.format("APPLES ({0}) x 200 = {1}", aCount, aCount * 200));
+    orange.setText(MessageFormat.format("ORANGES ({0}) x 80 = {1}", oCount, oCount * 200));
+    banana.setText(MessageFormat.format("BANANA ({0}) x 50 = {1}", bCount, bCount * 200));
+    mango.setText(MessageFormat.format("MANGOES ({0}) x 60 = {1}", mCount, mCount * 200));
+    grape.setText(MessageFormat.format("GRAPES ({0}) x 100 = {1}", gCount, gCount * 200));
+    total.setText(MessageFormat.format("Total : Rs{0}", tot));
 
 
   }
--- app/src/main/java/com/urk17cs290/shoppingcart/MainActivity.java
+++ app/src/main/java/com/urk17cs290/shoppingcart/MainActivity.java
@@ -1,11 +1,7 @@
 package com.urk17cs290.shoppingcart;
-
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -46,183 +42,140 @@ public class MainActivity extends AppCompatActivity {
         final int[] mangoCount={0};
 
 
-        MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid);
+        final MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid);
         list= findViewById(R.id.list);
         list.setAdapter(adapter);
     list.setOnItemClickListener(
-        new AdapterView.OnItemClickListener() {
+            (parent, view, position, id) -> {
+                if (position == 0) {
 
-          @Override
-          public void onItemClick(
-              final AdapterView<?> parent, final View view, int position, long id) {
-//
-//            try {
-              if (position == 0) {
-                //              Log.e("TAG", "inside pos 0");
+                  AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                  builder
+                      .setMessage("Adding to cart")
+                      .setTitle("Confirm")
+                      .setPositiveButton(
+                          "Yes",
+                              (dialog, which) -> {
+                                appleCount[0]++;
+                                total[0] += 200;
+                                Toast.makeText(
+                                        MainActivity.this, appleCount[0]+" Apples added to cart", Toast.LENGTH_SHORT)
+                                    .show();
+                                Log.e("TAG", "total: " + total[0]);
+                                Log.e("TAG", "apple: " + appleCount[0]);
+                              })
+                      .setNegativeButton(
+                          "No",
+                              (dialog, which) -> {
+                                // doing nothing
+                              })
+                      .show();
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder
-                    .setMessage("Adding to cart")
-                    .setTitle("Confirm")
-                    .setPositiveButton(
-                        "Ok",
-                        new DialogInterface.OnClickListener() {
-                          @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                            appleCount[0]++;
-                            total[0] += 200;
-                            Toast.makeText(
-                                    MainActivity.this, "1 Apple added to cart", Toast.LENGTH_SHORT)
-                                .show();
-                            Log.e("TAG", "total: " + total[0]);
-                            Log.e("TAG", "apple: " + appleCount[0]);
-                          }
-                        })
-                    .setNegativeButton(
-                        "No",
-                        new DialogInterface.OnClickListener() {
-                          @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                            // doing nothing
-                          }
-                        })
-                    .show();
+                } else if (position == 1) {
 
-              } else if (position == 1) {
+                  AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                  builder
+                      .setMessage("Adding to cart")
+                      .setPositiveButton(
+                          "Yes",
+                              (dialog, which) -> {
+                                orangeCount[0]++;
+                                total[0] += 80;
+                                Toast.makeText(
+                                        MainActivity.this, orangeCount[0]+" Oranges added to cart", Toast.LENGTH_SHORT)
+                                    .show();
+                                Log.e("TAG", "total: " + total[0]);
+                                Log.e("TAG", "apple: " + orangeCount[0]);
+                              })
+                      .setNegativeButton(
+                          "No",
+                              (dialog, which) -> {
+                                // doing nothing
+                              })
+                      .show();
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder
-                    .setMessage("Adding to cart")
-                    .setPositiveButton(
-                        "Ok",
-                        new DialogInterface.OnClickListener() {
-                          @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                            orangeCount[0]++;
-                            total[0] += 80;
-                            Toast.makeText(
-                                    MainActivity.this, "1 Orange added to cart", Toast.LENGTH_SHORT)
-                                .show();
-                            Log.e("TAG", "total: " + total[0]);
-                            Log.e("TAG", "apple: " + orangeCount[0]);
-                          }
-                        })
-                    .setNegativeButton(
-                        "No",
-                        new DialogInterface.OnClickListener() {
-                          @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                            // doing nothing
-                          }
-                        })
-                    .show();
+                } else if (position == 2) {
 
-              } else if (position == 2) {
+                  AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                  builder
+                      .setMessage("Adding to cart")
+                      .setPositiveButton(
+                          "Yes",
+                              (dialog, which) -> {
+                                bananaCount[0]++;
+                                total[0] += 50;
+                                Toast.makeText(
+                                        MainActivity.this, bananaCount[0]+" Banana added to cart", Toast.LENGTH_SHORT)
+                                    .show();
+                                Log.e("TAG", "total: " + total[0]);
+                                Log.e("TAG", "apple: " + bananaCount[0]);
+                              })
+                      .setNegativeButton(
+                          "No",
+                              (dialog, which) -> {
+                                // doing nothing
+                              })
+                      .show();
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder
-                    .setMessage("Adding to cart")
-                    .setPositiveButton(
-                        "Ok",
-                        new DialogInterface.OnClickListener() {
-                          @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                            bananaCount[0]++;
-                            total[0] += 50;
-                            Toast.makeText(
-                                    MainActivity.this, "1 Banana added to cart", Toast.LENGTH_SHORT)
-                                .show();
-                            Log.e("TAG", "total: " + total[0]);
-                            Log.e("TAG", "apple: " + bananaCount[0]);
-                          }
-                        })
-                    .setNegativeButton(
-                        "No",
-                        new DialogInterface.OnClickListener() {
-                          @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                            // doing nothing
-                          }
-                        })
-                    .show();
+                } else if (position == 3) {
 
-              } else if (position == 3) {
+                  AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                  builder
+                      .setMessage("Adding to cart")
+                      .setPositiveButton(
+                          "Yes",
+                              (dialog, which) -> {
+                                mangoCount[0]++;
+                                total[0] += 60;
+                                Toast.makeText(
+                                        MainActivity.this, mangoCount[0]+" Mango added to cart", Toast.LENGTH_SHORT)
+                                    .show();
+                                Log.e("TAG", "total: " + total[0]);
+                                Log.e("TAG", "apple: " + mangoCount[0]);
+                              })
+                      .setNegativeButton(
+                          "No",
+                              (dialog, which) -> {
+                                // doing nothing
+                              })
+                      .show();
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder
-                    .setMessage("Adding to cart")
-                    .setPositiveButton(
-                        "Ok",
-                        new DialogInterface.OnClickListener() {
-                          @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                            mangoCount[0]++;
-                            total[0] += 60;
-                            Toast.makeText(
-                                    MainActivity.this, "1 Mango added to cart", Toast.LENGTH_SHORT)
-                                .show();
-                            Log.e("TAG", "total: " + total[0]);
-                            Log.e("TAG", "apple: " + mangoCount[0]);
-                          }
-                        })
-                    .setNegativeButton(
-                        "No",
-                        new DialogInterface.OnClickListener() {
-                          @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                            // doing nothing
-                          }
-                        })
-                    .show();
+                } else if (position == 4) {
 
-              } else if (position == 4) {
+                  AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                  builder
+                      .setMessage("Adding to cart")
+                      .setPositiveButton(
+                          "Yes",
+                              (dialog, which) -> {
+                                grapeCount[0]++;
+                                total[0] += 100;
+                                Toast.makeText(
+                                        MainActivity.this, grapeCount[0]+" Grape added to cart", Toast.LENGTH_SHORT)
+                                    .show();
+                                Log.e("TAG", "total: " + total[0]);
+                                Log.e("TAG", "apple: " + grapeCount[0]);
+                              })
+                      .setNegativeButton(
+                          "No",
+                              (dialog, which) -> {
+                                  // doing nothing
+                              })
+                      .show();
+                }
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder
-                    .setMessage("Adding to cart")
-                    .setPositiveButton(
-                        "Ok",
-                        new DialogInterface.OnClickListener() {
-                          @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                            grapeCount[0]++;
-                            total[0] += 100;
-                            Toast.makeText(
-                                    MainActivity.this, "1 Grape added to cart", Toast.LENGTH_SHORT)
-                                .show();
-                            Log.e("TAG", "total: " + total[0]);
-                            Log.e("TAG", "apple: " + grapeCount[0]);
-                          }
-                        })
-                    .setNegativeButton(
-                        "No",
-                        new DialogInterface.OnClickListener() {
-                        @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                            // doing nothing
-                          }
-                        })
-                    .show();
-              }
-//            } catch (Exception e) {
-//              Log.e("TAG", "Exception : "+e.getMessage());
-//            }
-          }
-        });
+            });
         Button check =findViewById(R.id.checkBill);
-        check.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,BillActivity.class);
+        check.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this,BillActivity.class);
 
-                i.putExtra("TOTAL",total[0]);
-                i.putExtra("A_COUNT",appleCount[0]);
-                i.putExtra("O_COUNT",orangeCount[0]);
-                i.putExtra("B_COUNT",bananaCount[0]);
-                i.putExtra("M_COUNT",mangoCount[0]);
-                i.putExtra("G_COUNT",grapeCount[0]);
-                startActivity(i);
-            }
+            i.putExtra("TOTAL",total[0]);
+            i.putExtra("A_COUNT",appleCount[0]);
+            i.putExtra("O_COUNT",orangeCount[0]);
+            i.putExtra("B_COUNT",bananaCount[0]);
+            i.putExtra("M_COUNT",mangoCount[0]);
+            i.putExtra("G_COUNT",grapeCount[0]);
+            startActivity(i);
         });
     }
 
